1f0e6149bb66b4baa6f55e72c36348045b934d7e
RaquelC20 <166951304+RaquelC20@users.noreply.github.com>
2026-05-06 15:28:05 -0600

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,15 @@
+// Configuración de Firebase para NITROPEAK
+const firebaseConfig = {
+  apiKey: "AIzaSyCmQvuZForGQIxafEcIfHFAsPyeqgB1by",
+  authDomain: "nitropeak-6bee4.firebaseapp.com",
+  projectId: "nitropeak-6bee4",
+  storageBucket: "nitropeak-6bee4.firebasestorage.app",
+  messagingSenderId: "498858670688",
+  appId: "1:498858670688:web:c6951939e2c6ab33f94905",
+  measurementId: "G-87SK8SC22D"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const storage = firebase.storage();
